0c2b47534508b5bd229867ead81ac9b4a5984a37
MongolianPoorman <allanxu12345@gmail.com>
2016-11-09 20:42:32 -0500

    Update ccc07s2.cpp

--- CCC/2007/stage-1/ccc07s2.cpp
+++ CCC/2007/stage-1/ccc07s2.cpp
@@ -1,66 +1,61 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-	string a;
-	while(true){
-		cin >> a; 
-		if (a=="CU")
-		{
-			cout << "see you" << endl;
-		}
-		else if (a==":-)")
-		{
-			cout << "I'm happy" << endl;	
-		}
-		else if (a==":-(")
-		{
-			cout << "I'm unhappy" << endl;	
-		}
-		else if (a==";-)")
-		{
-			cout << "wink" << endl;	
-		}
-		else if (a==":-P")
-		{
-			cout << "stick out my tongue" << endl;	
-		}
-		else if (a=="(~.~)")
-		{
-			cout << "sleepy" << endl;	
-		}
-		else if (a=="TA")
-		{
-			cout << "totally awesome" << endl;	
-		}
-		else if (a=="CCC")
-		{
-			cout << "Canadian Computing Competition" << endl;	
-		}
-		else if (a=="CUZ")
-		{
-			cout << "because" << endl;	
-		}
-		else if (a=="TY")
-		{
-			cout << "thank-you" << endl;	
-		}
-		else if (a=="YW")
-		{
-			cout << "you're welcome" << endl;	
-		}
-		else if (a=="TTYL")
-		{
-			cout << "talk to you later" << endl;
-			break;
-		}
-		else
-		{
-			cout << a << endl;
-		}
-		
-		
+	int a, b, t; 
+	cin >> a; 
+	bool fit;
+	vector <int> box[a];
+	for (int i = 0; i < a; i++)
+	{
+		cin >> t; 
+		box[i].push_back(t);
+		cin >> t; 
+		box[i].push_back(t);
+		cin >> t; 
+		box[i].push_back(t);
+		t = box[i][0]*box[i][1]*box[i][2];
+		box[i].push_back(t);
+		reverse(box[i].begin(), box[i].end());
 	}
+	sort (box, box+a);
+	for (int i = 0 ; i<a; i++)
+	{
+	sort (box[i].begin(), box[i].end());
+	}
+	cin >> b; 
+	vector <int> good[b];
+	for(int i = 0; i < b; i++)
+	{
+		cin >> t; 
+		good[i].push_back(t);
+		cin >> t; 
+		good[i].push_back(t);
+		cin >> t; 
+		good[i].push_back(t);
+		sort(good[i].begin(), good[i].end());
+	}
+	
+	for(int i = 0; i < b; i++)
+	{
+		fit = false;
+		for (int j = 0; j < a; j++)
+		{
+			if (good[i][0] <= box[j][0]&&good[i][1] <= box[j][1]&&good[i][2] <= box[j][2])
+			{
+				cout << box[j][3] << endl;
+				fit = true;
+				break;
+			}
+			
+		}
+		if (!fit)
+			{
+				cout << "Item does not fit." << endl;
+			}
+	}
+	
 	return 0;
 }
