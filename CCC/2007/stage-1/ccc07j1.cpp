556267f132a8fca24fb0826fec43c891cf669c8f
MongolianPoorman <allanxu12345@gmail.com>
2016-11-09 20:40:56 -0500

    Create ccc07j1.cpp

---
+++ CCC/2007/stage-1/ccc07j1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, x; 
+    cin >> a >> b >> c ; 
+    
+    if ( a > b && a < c || a < b && a > c)
+        cout << a;
+        
+    if ( b > a && b < c || b < a && b > c)
+        cout << b;
+        
+    if ( c > b && c < a || c < b && c > a)
+        cout << c;
+  
+    
+   return 0;
+}
