8bae6d988ecedf73b945f914060ca90bd34e3759
MongolianPoorman <allanxu12345@gmail.com>
2016-11-09 20:51:13 -0500

    Create ccc09j1.cpp

---
+++ CCC/2009/stage-1/ccc09j1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a, b, c, t;
+   cin >> a >> b >> c;
+   t = a + b * 3 + c;
+   cout << "The 1-3-sum is " << t+91 << endl;
+   
+   return 0;
+}
