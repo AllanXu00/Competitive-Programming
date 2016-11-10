ac244882cc3e93431af6153d3b5b1d6698ac4116
MongolianPoorman <allanxu12345@gmail.com>
2016-11-09 20:26:45 -0500

    Create ccc04j1.cpp

---
+++ CCC/2004/stage-1/ccc04j1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h> 
+using namespace std;
+
+int main()
+{
+    int a, x; 
+    cin >> a;
+    x = sqrt (a);
+    cout  << "The largest square has side length " << x << "." ;
+      
+   return 0;
+}
